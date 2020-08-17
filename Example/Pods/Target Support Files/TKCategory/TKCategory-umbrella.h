94d5230a09d94eaf7fcbb9ae2fe770264e779797
hanxiuhui <hanxiuhui@offcn.com>
2020-08-17 14:54:39 +0800

    add category

--- Example/Pods/Target Support Files/TKCategory/TKCategory-umbrella.h
+++ Example/Pods/Target Support Files/TKCategory/TKCategory-umbrella.h
@@ -10,6 +10,11 @@
 #endif
 #endif
 
+#import "NSObject+TKAdd.h"
+#import "NSString+TKAdd.h"
+#import "UIButton+TKAdd.h"
+#import "UIColor+TKAdd.h"
+#import "UIViewController+TKAdd.h"
 
 FOUNDATION_EXPORT double TKCategoryVersionNumber;
 FOUNDATION_EXPORT const unsigned char TKCategoryVersionString[];
